b33e9d586c172971ae1e00eb1dfd011a5c6d28b3
mathlete01 <msallin@gmail.com>
2020-12-03 09:53:42 -0800

    CaUpdate current player in db with their newly entered name and final score

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #get 'players', to: 'players#index'
+  
 
   resources :players
+  get "players", to: "players#index"
+  post "players", to: "players#create"
+  patch "players", to: "players#update"
+  
 
 end
