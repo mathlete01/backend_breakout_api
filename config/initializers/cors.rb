eb28dd8e7e866c2be764b68c09b52de24ab7623b
Matty Sallin <matty@mathlete.com>
2025-01-28 21:23:23 -0800

    Fix CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-
-    resource '*',
+    origins 'https://www.qwertyball.com' # replace with your frontend URL
+    resource '/games',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
